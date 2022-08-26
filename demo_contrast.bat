ee8a77c6b6e1238d870d906f6256891d86ebfc1f
Andrew-Eathan <42521608+Andrew-Eathan@users.noreply.github.com>
2022-08-26 17:22:46 +0300

    delete contrast demo

--- demo_contrast.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p input="Input Filename > "
-set /p seed="Seed > "
-node pngfucker.js -input %input% -seed %seed% -regions 2 -shift -1 -crunch 40 -iquality 1 -splits 2 -div 1 -oquality 20 -contrast 1
-echo Done :D
-pause
